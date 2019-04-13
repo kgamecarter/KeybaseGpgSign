416e2912ff0865f8a3974bdbd6c589f5f2f06551
kgame <kgamecarter@gmail.com>
2019-04-13 13:38:02 +0800

    加入專案檔案。

---
+++ KeybaseGpgSign/MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace KeybaseGpgSign
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbCmd = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbCmd
+            // 
+            this.tbCmd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCmd.Location = new System.Drawing.Point(12, 12);
+            this.tbCmd.Multiline = true;
+            this.tbCmd.Name = "tbCmd";
+            this.tbCmd.Size = new System.Drawing.Size(397, 360);
+            this.tbCmd.TabIndex = 0;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(334, 378);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.BtnRun_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 413);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tbCmd);
+            this.Name = "MainForm";
+            this.Text = "Keybase GPG sign";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbCmd;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ KeybaseGpgSign/MainForm.cs
@@ -0,0 +1,102 @@
+﻿using CurlSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeybaseGpgSign
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+            btnRun.Enabled = false;
+            var lines = tbCmd.Lines;
+            var sb = new StringBuilder();
+            sb.Append(lines[2].Substring(3));
+            sb.Remove(5, 1);
+            sb.Length -= 5;
+            sb.Append(" |");
+            sb.Append(lines[3].Substring(2).Replace("'", ""));
+            sb.Length -= 4;
+            var psi = new ProcessStartInfo("cmd")
+            {
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            var p = Process.Start(psi);
+            var input = p.StandardInput;
+            input.WriteLine(sb.ToString());
+            input.WriteLine("exit");
+            string line;
+            sb.Clear();
+            while ((line = p.StandardOutput.ReadLine()) != null)
+            {
+                if (line == "-----END PGP MESSAGE-----")
+                {
+                    sb.Append("-----END PGP MESSAGE-----");
+                    p.StandardOutput.Close();
+                    break;
+                }
+                else if (sb.Length != 0)
+                {
+                    sb.AppendLine(line);
+                }
+                else if (line == "-----BEGIN PGP MESSAGE-----")
+                    sb.AppendLine("-----BEGIN PGP MESSAGE-----");
+            }
+            var result = sb.ToString();
+
+            var formDataDictionary = new Dictionary<string, string>()
+            {
+                ["sig"] = result,
+            };
+            string url = null;
+            for (int i = 4; i < lines.Length; i++)
+            {
+                if (lines[i].Contains("--data-urlencode"))
+                {
+                    line = lines[i].Substring(19, lines[i].Length - 22);
+                    var data = line.Split(new string[] { "=\"" }, StringSplitOptions.None);
+                    formDataDictionary.Add(data[0], data[1]);
+                }
+                else if (lines[i].Contains("https://keybase.io/"))
+                {
+                    url = lines[i].Trim();
+                }
+            }
+            var formData = new FormUrlEncodedContent(formDataDictionary);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var t = client.PostAsync(url, formData);
+                t.Wait();
+                var t2 = t.Result.Content.ReadAsStringAsync();
+                t2.Wait();
+                result = t2.Result;
+            }
+            MessageBox.Show(result);
+            tbCmd.Focus();
+            tbCmd.SelectAll();
+            btnRun.Enabled = true;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            tbCmd.Focus();
+        }
+    }
+}
